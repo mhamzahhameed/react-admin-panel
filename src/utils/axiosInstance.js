e894f273df2c9e26ec498268245089b7c556da86
mhamzahhameed <hamzahraja11@gmail.com>
2023-06-21 14:30:13 +0500

    conflick resolve

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iis5MjMwNzg0ODg5MDMiLCJzdWIiOjEsImlhdCI6MTY4NzMzMTc4NiwiZXhwIjoxNjg3NDE4MTg2fQ.50-67_IX3AgIPRMmTm7Q2VmtmEsnHy5AmSZg5FkKAC4';
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iis5MjMwNzg0ODg5MDMiLCJzdWIiOjEsImlhdCI6MTY4NzMzNjU4MiwiZXhwIjoxNjg3NDIyOTgyfQ.kFALbrLHQPkMvcex1wjCEpSvNyOHoOEAVOhBNyjKjfY";
 const AxiosInstance = axios.create({
     baseURL: 'http://3.133.0.29', 
     timeout: 5000, 
