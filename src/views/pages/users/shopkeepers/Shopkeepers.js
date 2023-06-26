5cd131f256c5c8b5f4b755f542712b5561e3acbe
mhamzahhameed <hamzahraja11@gmail.com>
2023-06-27 01:08:56 +0500

    update token

--- src/views/pages/users/shopkeepers/Shopkeepers.js
+++ src/views/pages/users/shopkeepers/Shopkeepers.js
@@ -19,7 +19,7 @@ const [code,setCode] = useState("");
     // eslint-disable-next-line
     }, [ ])
   const fetchData = async () => {
-    let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iis5MjMwNzg0ODg5MDMiLCJzdWIiOjEsImlhdCI6MTY4NzY5MTM1OSwiZXhwIjoxNjg3Nzc3NzU5fQ.PdJULKkhSwRBOmZtAIRso55XkXplL5nL_-zLxs1Ba9I';
+    let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iis5MjMwNzg0ODg5MDMiLCJzdWIiOjEsImlhdCI6MTY4Nzc5OTMyMCwiZXhwIjoxNjg3ODg1NzIwfQ.xyM4Ha6iDlnSVqdI5jNQ2YQOJgdW0mgiigTT88HWU4A';
     try {
       let count = await AxiosInstance.get(`/api/user`,{
         headers:{
