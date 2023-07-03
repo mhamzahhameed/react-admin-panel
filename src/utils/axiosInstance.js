da71e81db537fe5c80fb3618cd87e4a22380d129
mhamzahhameed <hamzahraja11@gmail.com>
2023-07-03 16:34:33 +0500

    token update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjAzMDEzMzMyMjIxIiwic3ViIjoxLCJpYXQiOjE2ODc5NDQwOTAsImV4cCI6MTY4ODAzMDQ5MH0.2_aAMfvtu5IF8teZYc5Bmiyi5sVguDK-EpNK8g18zYw';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjAzMDEzMzMyMjIxIiwic3ViIjoxLCJpYXQiOjE2ODgzODQwMTEsImV4cCI6MTY4ODQ3MDQxMX0.4W_naTWSLfLesgNjKevx95W4Ux-47mslsyXAYlkP43A';
 const AxiosInstance = axios.create({
     baseURL: 'https://api.sehrapp.com', 
     headers: {
