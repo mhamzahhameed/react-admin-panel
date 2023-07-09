c7fb6b8d397488c7b7ef8722c9824108067e962c
ShahabImtiaz <shahabimtiaz03@gmail.com>
2023-07-09 18:10:03 +0500

    sehr code generate button algo

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjAzMDEzMzMyMjIxIiwic3ViIjoxLCJpYXQiOjE2ODg4MTkzOTEsImV4cCI6MTY4ODkwNTc5MX0.7cCkZyLxOQd1hF5eqFWA7LRDISHNaEOAE4kVWNRksMQ';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjAzMDEzMzMyMjIxIiwic3ViIjoxLCJpYXQiOjE2ODg4ODE1MzEsImV4cCI6MTY4ODk2NzkzMX0.JB0BRsPf2dz9oB-oM8JxkEWVIU3-1OU8-gOTjfwTu_Y';
 const AxiosInstance = axios.create({
     baseURL: 'https://api.sehrapp.com', 
     headers: {
