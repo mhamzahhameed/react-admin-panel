da369cc84837d699efcdfae2eba4a7ef20c835cd
mhamzahhameed <hamzahraja11@gmail.com>
2023-07-29 18:53:56 +0500

    remove warnings

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 
 import {
-  CAvatar,
-  CButton,
-  CButtonGroup,
+  // CAvatar,
+  // CButton,
+  // CButtonGroup,
   CCard,
   CCardBody,
   CCardFooter,
@@ -11,52 +11,52 @@ import {
   CCol,
   CProgress,
   CRow,
-  CTable,
-  CTableBody,
-  CTableDataCell,
-  CTableHead,
-  CTableHeaderCell,
-  CTableRow,
+  // CTable,
+  // CTableBody,
+  // CTableDataCell,
+  // CTableHead,
+  // CTableHeaderCell,
+  // CTableRow,
 } from '@coreui/react'
-import { CChartLine } from '@coreui/react-chartjs'
-import { getStyle, hexToRgba } from '@coreui/utils'
-import CIcon from '@coreui/icons-react'
-import {
-  cibCcAmex,
-  cibCcApplePay,
-  cibCcMastercard,
-  cibCcPaypal,
-  cibCcStripe,
-  cibCcVisa,
-  cibGoogle,
-  cibFacebook,
-  cibLinkedin,
-  cifBr,
-  cifEs,
-  cifFr,
-  cifIn,
-  cifPl,
-  cifUs,
-  cibTwitter,
-  cilCloudDownload,
-  cilPeople,
-  cilUser,
-  cilUserFemale,
-} from '@coreui/icons'
+// import { CChartLine } from '@coreui/react-chartjs'
+// import { getStyle, hexToRgba } from '@coreui/utils'
+// import CIcon from '@coreui/icons-react'
+// import {
+//   cibCcAmex,
+//   cibCcApplePay,
+//   cibCcMastercard,
+//   cibCcPaypal,
+//   cibCcStripe,
+//   cibCcVisa,
+//   cibGoogle,
+//   cibFacebook,
+//   cibLinkedin,
+//   cifBr,
+//   cifEs,
+//   cifFr,
+//   cifIn,
+//   cifPl,
+//   cifUs,
+//   cibTwitter,
+//   cilCloudDownload,
+//   cilPeople,
+//   cilUser,
+//   cilUserFemale,
+// } from '@coreui/icons'
 
-import avatar1 from 'src/assets/images/avatars/1.jpg'
-import avatar2 from 'src/assets/images/avatars/2.jpg'
-import avatar3 from 'src/assets/images/avatars/3.jpg'
-import avatar4 from 'src/assets/images/avatars/4.jpg'
-import avatar5 from 'src/assets/images/avatars/5.jpg'
-import avatar6 from 'src/assets/images/avatars/6.jpg'
+// import avatar1 from 'src/assets/images/avatars/1.jpg'
+// import avatar2 from 'src/assets/images/avatars/2.jpg'
+// import avatar3 from 'src/assets/images/avatars/3.jpg'
+// import avatar4 from 'src/assets/images/avatars/4.jpg'
+// import avatar5 from 'src/assets/images/avatars/5.jpg'
+// import avatar6 from 'src/assets/images/avatars/6.jpg'
 
-import WidgetsBrand from '../widgets/WidgetsBrand'
+// import WidgetsBrand from '../widgets/WidgetsBrand'
 import WidgetsDropdown from '../widgets/WidgetsDropdown'
 import AxiosInstance from 'src/utils/axiosInstance'
 
 const Dashboard = () => {
-  const [loader, setLoader] = useState(true)
+  // const [loader, setLoader] = useState(true)
   const [userdata, setUserData] = useState([])
   const [totalSehrData, setTotalSehrData] = useState([])
   const [totalPaid, setTotalPaid] = useState(0)
@@ -269,7 +269,7 @@ const Dashboard = () => {
       setTotalSehrData(business)
       setTotalPayment(totalPayment)
       setTotalPaid(totalPaid)
-      setLoader(false)
+      // setLoader(false)
     } catch (error) {
       console.error(error)
     }
